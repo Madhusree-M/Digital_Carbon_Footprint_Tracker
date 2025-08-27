9c9c359c83035522e84adbf7c69f3cef96bf135e
Madhusree M <mmadhusreemayilsamy@gmail.com>
2025-08-28 01:01:14 +0530

    Basic structure of carbontracker/model

---
+++ src/com/carbontracker/model/Activity.java
@@ -0,0 +1,5 @@
+package com.carbontracker.model;
+
+public class Activity {
+    
+}
---
+++ src/com/carbontracker/model/ElectricityActivity.java
@@ -0,0 +1,11 @@
+package com.carbontracker.model;
+
+import java.util.*;
+
+public class ElectricityActivity {
+    Scanner sc = new Scanner(System.in);
+    // System.out.print("Enter electricity units : ");
+    
+
+    
+}
