720348d3f3e855cb8796dc5d88503499f0555851
Madhusree M <mmadhusreemayilsamy@gmail.com>
2025-09-03 22:58:32 +0530

    Updated ElectricityActivity file

--- src/com/carbontracker/model/ElectricityActivity.java
+++ src/com/carbontracker/model/ElectricityActivity.java
@@ -1,11 +1,36 @@
 package com.carbontracker.model;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class ElectricityActivity {
-    Scanner sc = new Scanner(System.in);
-    // System.out.print("Enter electricity units : ");
-    
+    private double units;   // electricity units consumed (kWh)
+    private final double EMISSION_FACTOR = 0.85; // kg CO2 per kWh
 
-    
+    // Method to input electricity data
+    public void inputElectricityUsage() 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter electricity units consumed (kWh): ");
+        units = sc.nextDouble();
+    }
+
+    // Method to calculate CO2 emissions
+    public double calculateEmissions() 
+    {
+        return units * EMISSION_FACTOR;
+    }
+
+    // Getter for units
+    public double getUnits() 
+    {
+        return units;
+    }
+
+    // Display results
+    public void displayResult() 
+    {
+        System.out.println("Electricity Usage: " + units + " kWh");
+        System.out.println("Estimated Emissions: " + calculateEmissions() + " kg CO2");
+
+    }
 }
