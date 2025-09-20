50442c23e764974f33cb659acb61fd8a2390c2c4
Madhusree M <mmadhusreemayilsamy@gmail.com>
2025-09-20 20:56:19 +0530

    Started

--- src/com/carbontracker/model/Admin.java
+++ src/com/carbontracker/model/Admin.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Admin {
     public static void main(String[] args) {
         Map<String,String> Users = new HashMap<>();
+        //Decided to use map because I need Key-value pairs
 
         System.out.println("1.Login");
         System.out.println("2.New User");
