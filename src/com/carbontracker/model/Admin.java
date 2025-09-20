bdcb78237c35e67082b352fc6c0246aedc4715a2
Madhusree M <mmadhusreemayilsamy@gmail.com>
2025-09-20 20:48:22 +0530

    Tried to add a new class admin

---
+++ src/com/carbontracker/model/Admin.java
@@ -0,0 +1,19 @@
+package com.carbontracker.model;
+
+import java.util.*;
+
+public class Admin {
+    public static void main(String[] args) {
+        Map<String,String> Users = new HashMap<>();
+
+        System.out.println("1.Login");
+        System.out.println("2.New User");
+
+        int choice;
+        Scanner sc = new Scanner(System.in);
+        choice = sc.nextInt();
+
+        
+    }
+
+}
