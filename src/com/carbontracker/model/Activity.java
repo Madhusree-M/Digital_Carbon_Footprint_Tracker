ab2d9a468761630af877b6dea362d900d975f99a
Madhusree M <mmadhusreemayilsamy@gmail.com>
2025-09-21 17:22:09 +0530

    feat - new datamember DATE

--- src/com/carbontracker/model/Activity.java
+++ src/com/carbontracker/model/Activity.java
@@ -1,10 +1,30 @@
 package com.carbontracker.model;
 import java.util.Scanner;
+import java.time.LocalDate;
 
 //abstract class
 public abstract class Activity {
     protected String type;
     public double emission; //mentioned as public because User class is using this datamember
+
+    private LocalDate date;
+
+    //Constructor
+    Activity()
+    {
+        date = LocalDate.now();
+        //By default , date will be set to Today
+    }
+    //Gtter
+    LocalDate getDate()
+    {
+        return date;
+    }
+    //Setter
+    void setDate(LocalDate date)
+    {
+        this.date = date;
+    }
 
     //Abstraction
     public abstract void inputData(Scanner sc);
